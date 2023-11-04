0a7de5c7ea974aa8fd3c25c6f107583070b8111d
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-04 12:45:37 +0900

    [design] 검색 결과 css

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -19,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function() {
             return 0;
         }
 
+        results.innerHTML = "";
+
         const query = {
             params: {
                 category: category,
@@ -34,10 +36,62 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log(response);
             console.log(typeof response.data);
 
-            getValuesArray(response.data);
+            const searchedArray = getValuesArray(response.data);
 
-            const jsonText = JSON.stringify(response.data, null, 2);
-            results.textContent = jsonText;
+            searchedArray.forEach(item => {
+                const div = document.createElement("div");
+                div.innerHTML = `
+                    <div class="w-full mb-4">
+                        <p class="w-full p-3 text-center text-xl bolder bg-slate-300">${item.icao}</p>
+                        <div class="w-full">
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">ICAO</p>
+                                <p class="flex-1">${item.icao}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">IATA</p>
+                                <p class="flex-1">${item.iata}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">NAME</p>
+                                <p class="flex-1">${item.name}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">CITY</p>
+                                <p class="flex-1">${item.city}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">STATE</p>
+                                <p class="flex-1">${item.state}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">COUNTRY</p>
+                                <p class="flex-1">${item.country}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">ELEVATION</p>
+                                <p class="flex-1">${item.elevation}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">LAT</p>
+                                <p class="flex-1">${item.lat}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">LON</p>
+                                <p class="flex-1">${item.lon}</p>
+                            </div>
+                            <div class="flex flex-row w-full">
+                                <p class="w-1/6 min-w-[100px] border-r border-slate-400">TZ</p>
+                                <p class="flex-1">${item.tz}</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                results.appendChild(div);
+            });
+
+            //const jsonText = JSON.stringify(response.data, null, 2);
+            //results.textContent = jsonText;
 
         } catch (error) {
             console.error("에러 발생:", error);
